a255eee485fe93b5e76f81c4be257fa626577cba
Andy Babay <andy.r.babay@nasa.gov>
2025-07-29 12:49:00 -0400

    Implemented method for extracting metadata out of geotiff source file

--- src/components/andy-experimental/andy-experimental.component.ts
+++ src/components/andy-experimental/andy-experimental.component.ts
@@ -22,9 +22,11 @@ export default class TerraAndyExperimental extends TerraElement {
   @property() attr = 'example';
 
 
-  firstUpdated() {
+  async firstUpdated() {
 
-    this.callHarmony()
+    // var harmony_resp = await this.callHarmony()
+    // console.log("Harmony resp: ",harmony_resp)
+    
     const url =
       'https://localhost:4000/dist/GIOVANNI-timeAvgMap.M2T1NXAER_5_12_4_BCCMASS.20090101-20090105.62E_5N_95E_40N.tif';
 
@@ -56,17 +58,54 @@ export default class TerraAndyExperimental extends TerraElement {
         projection: 'EPSG:3857', // match OSM (Web Mercator)
       }),
     });
-    // Fetch Metadata once view has been created
-    gtSource.getView().then((view) => {
-      console.log(view)
 
+    // Fetch Metadata once view has been created. These attributes are private and not public to the API but are needed for metadata retrieval
+    this.fetchGeotiffMetadata(gtSource)
+
+  }
+
+  fetchGeotiffMetadata(gtSource: GeoTIFF) {
+    gtSource.getView().then(() => {
+      const internal = gtSource as any;
+      console.log('sourceImagery_:', internal.sourceImagery_);
+      const gtImage = internal.sourceImagery_[0][0];
+      const gtMetadata = gtImage.fileDirectory?.GDAL_METADATA;
+      console.log(typeof (gtMetadata))
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(gtMetadata, "application/xml");
+      // Get all <Item> elements
+      const items = xmlDoc.querySelectorAll("Item");
+      console.log("items: ",items)
+
+      const dataObj: { [key: string]: string } = {};
+
+      for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        const name = item.getAttribute("name");
+        const value = item.textContent ? item.textContent.trim() : "";
+        if(name) {
+        dataObj[name] = value
+        }
+      }
+      // const description = dataObj["DESCRIPTION"]
+      // const conventions = dataObj["Conventions"]
+      // const DOI = dataObj["DOI"]
+      // const cell_methods = dataObj["cell_methods"]
+      // const latitude_resolution = dataObj["latitude_resolution"]
+      // const long_name = dataObj["long_name"]
+      // const product_short_name = dataObj["product_short_name"]
+      // const product_version = dataObj["product_version"]
+      // const units = dataObj["units"]
+      // const userenddate = dataObj["userenddate"]
+      // const userstartdate = dataObj['userstartdate']
+      console.log("Data obj: ",dataObj)
+      return dataObj
     })
-
   }
 
   async callHarmony() {
     var geotiff_url = 'https://lpo4uv7f0h.execute-api.us-east-1.amazonaws.com/default/harmony-link-proxy?url=https://harmony.uat.earthdata.nasa.gov/service-results/harmony-uat-staging/public/a2762914-4e10-402c-b0ee-504ca508c8b4/7696240/GIOVANNI-timeAvgMap.M2T1NXAER_5_12_4_BCCMASS.20090101-20090105.62E_5N_95E_40N.tif'
-      const token = 'redacted'
+     const token = ''
       var response = await fetch(geotiff_url, {
           headers: {
             Authorization: `Bearer ${token}`
